7ad3961c9efc64d656fc0c0c5b5c8e08bb9377e5
Kaitlyn Huynh <kahu1074@colorado.edu>
2023-02-15 16:39:12 -0700

    added curved header and changed colors

--- feasibility-test/src/Home.js
+++ feasibility-test/src/Home.js
@@ -1,12 +1,54 @@
-import { useState, useCallback } from "react";
+import './Website.scss';
+import ReactCurvedText from "react-curved-text";
+import React, { useState, useCallback } from "react";
 import Navigation from './Navigation.js';
 function Home(){
+    const [width, setWidth] = useState(1000);
+    const [height, setHeight] = useState(551);
+    const [cx, setCx] = useState(566);
+    const [cy, setCy] = useState(300); //moves it down
+    const [rx, setRx] = useState(377);
+    const [ry, setRy] = useState(90); //changes how bent it is
+    const [startOffset, setStartOffset] = useState(10);
+    const [reversed, setReversed] = useState(true);
+    const [text, setText] = useState("P l a s t i f a s h i o n");
+    const [fontSize, setFontSize] = useState(80);
+    const [kerning, setKerning] = useState()
+    const font = "Recoleta Bold";
+    const [textPathFill, setTextPathFill] = useState();
+    const [dy, setDy] = useState(-60);
+    const [fill, setFill] = useState();
+    const [rotate, setRotate] = useState(5);
+    let moveUp = {
+        marginTop: '-20px',
+    };
+  
+    const textProps = fontSize ? { style: { fontSize: fontSize, fontFamily: font } } : null;
+    const textPathProps = textPathFill ? { fill: textPathFill } : null;
+    const tspanProps = dy ? { dy: dy } : null;
+    const ellipseProps = fill ? { style: `fill: ${fill}` } : null;
+    const svgProps = rotate
+      ? { style: { transform: `rotate(${rotate}deg)` } }
+      : null;
     return(
         <div>
             <Navigation />
-            <h1>
-                This is an example app!
-            </h1>
+                <ReactCurvedText
+                    width={width}
+                    height={height}
+                    cx={cx}
+                    cy={cy}
+                    rx={rx}
+                    ry={ry}
+                    startOffset={startOffset}
+                    reversed={reversed}
+                    text={text}
+                    textProps={textProps}
+                    textPathProps={{"fill": "white"}}
+                    tspanProps={tspanProps}
+                    ellipseProps={ellipseProps}
+                    svgProps={svgProps}
+                />
         </div>
     );
 }
--- feasibility-test/src/Navigation.js
+++ feasibility-test/src/Navigation.js
@@ -2,17 +2,20 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './Website.scss';
 
 function Navigation(){
     return(
-        <Navbar bg="light" expand="lg">
+        <Navbar bg="light" expand="lg" className = "navbar-white">
         <Container>
-          <Navbar.Brand href="#home">Feasibility Test</Navbar.Brand>
+          {/* <Navbar.Brand href="#home" className = "menu-item">Our Story</Navbar.Brand> */}
+          
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">About</Nav.Link>
+              <Nav.Link href="#home" className = "menu-item">Our Story</Nav.Link>
+              <Nav.Link href="#home" className = "menu-item">Home</Nav.Link>
+              <Nav.Link href="#link" className = "menu-item" >About</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
