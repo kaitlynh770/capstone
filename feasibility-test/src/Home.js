402e7b5161b35c6d7bd6002e917f1e3fd565e94a
Kaitlyn Huynh <kahu1074@colorado.edu>
2023-02-22 17:08:53 -0700

    added images to homepage

--- feasibility-test/src/Home.js
+++ feasibility-test/src/Home.js
@@ -2,6 +2,12 @@ import './Website.scss';
 import ReactCurvedText from "react-curved-text";
 import React, { useState, useCallback } from "react";
 import Navigation from './Navigation.js';
+import dress from './img/dress.png';
+import fashionIcon from './img/fashion-icon.png';
+import threadIcon from './img/thread-icon.png';
+import environmentIcon from './img/environment-icon.png';
+import processIcon from './img/process-icon.png';
+import teamworkIcon from './img/teamwork-icon.png';
 function Home(){
     const [width, setWidth] = useState(1000);
     const [height, setHeight] = useState(551);
@@ -19,10 +25,6 @@ function Home(){
     const [dy, setDy] = useState(-60);
     const [fill, setFill] = useState();
     const [rotate, setRotate] = useState(5);
-    let moveUp = {
-        marginTop: '-20px',
-    };
-  
     const textProps = fontSize ? { style: { fontSize: fontSize, fontFamily: font } } : null;
     const textPathProps = textPathFill ? { fill: textPathFill } : null;
     const tspanProps = dy ? { dy: dy } : null;
@@ -49,6 +51,12 @@ function Home(){
                     ellipseProps={ellipseProps}
                     svgProps={svgProps}
                 />
+                <img src = {dress} className='overlap-short-term'/>
+                <img src = {fashionIcon} className='left icon-size' />
+                <img src = {threadIcon} className='right icon-size' />
+                <img src = {teamworkIcon} className = 'left-lower icon-size' />
+                <img src = {processIcon} className = 'right-lower icon-size' />
+                <img src = {environmentIcon} className = 'center-icon icon-size' />
         </div>
     );
 }
