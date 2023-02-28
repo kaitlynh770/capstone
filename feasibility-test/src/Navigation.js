2bb653f2f852c27ef049cf066beee0502558beae
Kaitlyn Huynh <kahu1074@colorado.edu>
2023-02-28 10:58:59 -0700

    added just a normal home tab for now, will probably insert special heading/logo later on

--- feasibility-test/src/Navigation.js
+++ feasibility-test/src/Navigation.js
@@ -35,6 +35,7 @@ const Navigation =  ({setHomeState, homeState, setProcessState, processState, se
   }
     return(
       <div className = 'top-bar'>
+        <NavBarItem text = "Home" active = {homeState} onClick = {clickHome} />
         <NavBarItem text = "Our Story" active = {storyState} onClick = {clickStory} />
         <NavBarItem text = "Process" active = {processState} onClick = {clickProcess} />
         <NavBarItem text = "Why us" active = {reasonsState} onClick = {clickReasons} />
