9cd2e3ec2faf05a010efab9bc82ad68e21bffc2c
Kaitlyn Huynh <kahu1074@colorado.edu>
2023-03-20 15:58:15 -0600

    added some animation to the homepage

--- feasibility-test/src/Home.js
+++ feasibility-test/src/Home.js
@@ -1,6 +1,6 @@
 import './Website.scss';
 import ReactCurvedText from "react-curved-text";
-import React, { useState, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import Navigation from './Navigation.js';
 import dress from './img/dress.png';
 import fashionIcon from './img/fashion-icon.png';
@@ -8,6 +8,12 @@ import threadIcon from './img/thread-icon.png';
 import environmentIcon from './img/environment-icon.png';
 import processIcon from './img/process-icon.png';
 import teamworkIcon from './img/teamwork-icon.png';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { gsap } from "gsap";
+import Tippy from "@tippy.js/react";
+import "tippy.js/dist/tippy.css";
+import "tippy.js/themes/light.css";
 function Home(){
     const [width, setWidth] = useState(1000);
     const [height, setHeight] = useState(551);
@@ -32,30 +38,41 @@ function Home(){
     const svgProps = rotate
       ? { style: { transform: `rotate(${rotate}deg)` } }
       : null;
+    useEffect(() => {
+        AOS.init();
+    }, [])
+    const onEnter = ({ currentTarget }) => {
+        gsap.to(currentTarget, {height: "7vw"});
+    };
+    const onLeave = ({ currentTarget }) => {
+        gsap.to(currentTarget, {height: "6vw"});
+    };
     return(
-        <div>
-            <ReactCurvedText
-                width={width}
-                height={height}
-                cx={cx}
-                cy={cy}
-                rx={rx}
-                ry={ry}
-                startOffset={startOffset}
-                reversed={reversed}
-                text={text}
-                textProps={textProps}
-                textPathProps={{"fill": "white"}}
-                tspanProps={tspanProps}
-                ellipseProps={ellipseProps}
-                svgProps={svgProps}
-            />
-            <img src = {dress} className='overlap-short-term'/>
-            <img src = {fashionIcon} className='left icon-size' />
-            <img src = {threadIcon} className='right icon-size' />
-            <img src = {teamworkIcon} className = 'left-lower icon-size' />
-            <img src = {processIcon} className = 'right-lower icon-size' />
-            <img src = {environmentIcon} className = 'center-icon icon-size' />
+        <div className = 'home'>
+            <div data-aos = "fade-down" data-aos-duration = "2000">
+                <ReactCurvedText
+                    width={width}
+                    height={height}
+                    cx={cx}
+                    cy={cy}
+                    rx={rx}
+                    ry={ry}
+                    startOffset={startOffset}
+                    reversed={reversed}
+                    text={text}
+                    textProps={textProps}
+                    textPathProps={{"fill": "white"}}
+                    tspanProps={tspanProps}
+                    ellipseProps={ellipseProps}
+                    svgProps={svgProps}
+                />
+            </div>
+            <img data-aos = "zoom-in" data-aos-duration = "1000" data-aos-delay = "600" src = {dress} className='overlap-short-term garment-size'/>
+            <img data-aos = "fade-down" data-aos-duration = "1500" data-aos-delay = "800" src = {fashionIcon} className='left icon-size' />
+            <img data-aos = "fade-down" data-aos-duration = "1500" data-aos-delay = "2800" src = {threadIcon} className='right icon-size' />
+            <img data-aos = "fade-down" data-aos-duration = "1500" data-aos-delay = "1300" src = {teamworkIcon} className = 'left-lower icon-size' />
+            <img data-aos = "fade-down" data-aos-duration = "1500" data-aos-delay = "2300" src = {processIcon} className = 'right-lower icon-size' />
+            <img data-aos = "fade-down" data-aos-duration = "1500" data-aos-delay = "1800"src = {environmentIcon} className = 'center-icon icon-size' />
         </div>
     );
 }
--- feasibility-test/src/Story.js
+++ feasibility-test/src/Story.js
@@ -1,14 +1,21 @@
 import './Website.scss';
 import React, { useState, useCallback } from "react";
-import fashionPic from './img/fashion-girlie.png'
+import fashionPic from './img/fashion-girlie.png';
+import { gsap } from "gsap";
 function Story(){
     let boldFont = {
         fontFamily: 'Recoleta Bold, cursive',
     };
+    const onEnter = ({ currentTarget }) => {
+        gsap.to(currentTarget, {color: "#6D94DC", scale: 1.2});
+    };
+    const onLeave = ({ currentTarget }) => {
+        gsap.to(currentTarget, { color: "white", scale: 1})
+    }
     return(
         <div className = 'container'>
             <div className = 'left-half'>
-            <h2 className = 'left-float-heading'>high class fashion made out of <span style = {boldFont}>recycled materials</span></h2>
+            <h2 className = 'left-float-heading' onMouseEnter = {onEnter} onMouseLeave={onLeave}>high class fashion made out of <span style = {boldFont}>recycled materials</span></h2>
                 <p className = 'about-paragraph'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
             <div className = 'fashion-pic'>
