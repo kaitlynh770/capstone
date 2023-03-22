b98b2852e4b2d3d204a8817e5857e75b0f9385ef
Kaitlyn Huynh <kahu1074@colorado.edu>
2023-03-22 17:59:40 -0600

    tooltips now trigger on hover over image

--- feasibility-test/src/Home.js
+++ feasibility-test/src/Home.js
@@ -82,24 +82,24 @@ function Home(){
                         <p className='icon-caption'>High Fashion</p>
                     </div>
                 </Tippy>
-                <Tippy theme = {"light"} content = {teamWorkText} animation = {"scale"} maxWidth={300}>
-                    <div data-aos = "fade-down" data-aos-duration = "1500" data-aos-delay = "1300" className = 'left-lower'>
-                        <img src = {teamworkIcon} className='icon-size' style = {secondTier} />
+                    <div data-aos = "fade-down" data-aos-duration = "1500" data-aos-delay = "1300" className = 'left-lower'  style = {secondTier}>
+                        <Tippy theme = {"light"} content = {teamWorkText} animation = {"scale"} maxWidth={300}>
+                            <img src = {teamworkIcon} className='icon-size' />
+                        </Tippy>
                         <p className='icon-caption'>Teamwork</p>
                     </div>
-                </Tippy>
-                <Tippy theme = {"light"} content = {environmentText} animation = {"scale"} maxWdith = {300}>
-                    <div data-aos = "fade-down" data-aos-duration = "1500" data-aos-delay = "1800" className = 'center'>
-                        <img src = {environmentIcon} className='icon-size' style = {thirdTier}/>
+                    <div data-aos = "fade-down" data-aos-duration = "1500" data-aos-delay = "1800" className = 'center' style = {thirdTier}>
+                        <Tippy theme = {"light"} content = {environmentText} animation = {"scale"} maxWdith = {300}>
+                            <img src = {environmentIcon} className='icon-size'/>
+                        </Tippy>
                         <p className='icon-caption'>Environment</p>
                     </div>
-                </Tippy>
-                <Tippy theme = {"light"} content = {processText} animation = {"scale"} maxWdith = {300}>
-                    <div data-aos = "fade-down" data-aos-duration = "1500" data-aos-delay = "2300" className = 'right-lower'>
-                        <img src = {processIcon} className='icon-size' style = {secondTier} />
+                    <div data-aos = "fade-down" data-aos-duration = "1500" data-aos-delay = "2300" className = 'right-lower' style = {secondTier}>
+                        <Tippy theme = {"light"} content = {processText} animation = {"scale"} maxWdith = {300}>
+                            <img src = {processIcon} className='icon-size' />
+                        </Tippy>
                         <p className='icon-caption'>Process</p>
                     </div>
-                </Tippy>
                 <Tippy theme = {"light"} content = {materialText} animation = {"scale"} maxWidth={300}>
                     <div data-aos = "fade-down" data-aos-duration = "1500" data-aos-delay = "2800" className='right'>
                         <img src = {threadIcon} className='icon-size' />
