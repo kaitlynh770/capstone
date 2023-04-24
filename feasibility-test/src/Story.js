bafa46d94ca411aa96d339f7155307f4a074b5d0
Kaitlyn Huynh <kahu1074@colorado.edu>
2023-04-24 11:34:33 -0600

    added video

--- feasibility-test/src/Story.js
+++ feasibility-test/src/Story.js
@@ -2,6 +2,7 @@ import './Website.scss';
 import React, { useState, useCallback } from "react";
 import fashionPic from './img/fashion-girlie.png';
 import AOS from 'aos';
+import process from './img/fast-process.mp4';
 import 'aos/dist/aos.css';
 function Story(){
     let boldFont = {
@@ -15,7 +16,8 @@ function Story(){
                 <p data-aos = "fade-down" data-aos-duration = "1500" data-aos-delay = "3000" className = 'about-paragraph'>A sustainable and elegant piece of clothing produced by deploying various engineering techniques. Plastifashion aims to do something rarely seen in the fashion industry: create high-end clothing out of recycled materials. With our dedication to the environment and a desire to make an impact in the world of high-fashion, Plastifashion is sure to shine a new light on recyclable materials.</p>
             </div>
             <div className = 'right-half'>
-                <img data-aos = "fade-up" data-aos-duration = "1500" data-aos-delay = "3000" className = 'pic-adjustments-story' src = {fashionPic} />
+                <video className = 'pic-adjustments-story' data-aos = "fade-up" data-aos-duration = "1500" data-aos-delay = "3000" src = {process} autoPlay = {true} muted={true} loop = {true} />
+                {/* <img data-aos = "fade-up" data-aos-duration = "1500" data-aos-delay = "3000" className = 'pic-adjustments-story' src = {fashionPic} /> */}
             </div>
         </div>
     );
